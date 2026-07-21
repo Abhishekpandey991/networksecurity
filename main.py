8bbcf2f03f9e3e35f09b400236f26aa5a7722624
student-colleg-cloud <abhishekpandey76840@gmail.com>
2026-07-22 00:51:43 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from networkSecurity.exception.exception import NetworkSecurityException
 from networkSecurity.logging.logger import logging
 
 from networkSecurity.components.data_ingestion import DataIngestion
-from networkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from networkSecurity.entity.config_entity import TrainingPipelineConfig
 from networkSecurity.components.data_validation import DataValidation
+from networkSecurity.components.data_transformation import DataTransformation
+
+from networkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networkSecurity.entity.config_entity import TrainingPipelineConfig
+
 
 
 if __name__=="__main__":
@@ -25,6 +28,15 @@ if __name__=="__main__":
     data_validation_artifact=data_validation.initiate_data_validation()
     logging.info("data Validation Completed")
     print(data_validation_artifact)
+
+
+    datatransformationconfig=DataTransformationConfig(trainingpipelineconfig)
+    data_transformation=DataTransformation(data_validation_artifact,datatransformationconfig)
+    logging.info("Initiate the data Transformation")
+    data_transformation_artifact=data_transformation.initiate_data_transformation()
+    logging.info("data Transformation Completed")
+    print(data_transformation_artifact)
+
 
 
   except Exception as e:
